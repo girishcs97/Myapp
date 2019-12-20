40ed521fe0230a7698258214e405c44d893befd4
Girish C s <gcs@global.sial.com>
2019-12-20 18:50:33 +0530

    initial commit;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  render() {
+    return (
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   <div className="App">
+     <h1>Counter Application</h1>
+    <h3>Current Count Value: {this.state.count}</h3>
+    <button className='inc' onClick={(e) => this.increment(e)}>Increment</button>
+  </div>
+    );
+  }
+};
 
-export default App;
+export default Counter;
